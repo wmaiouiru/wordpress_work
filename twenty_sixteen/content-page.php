c853300f3e49ab498f2b29e52776881fbde596b8
wmaiouiru <wmaiouiru@gmail.com>
2016-09-02 11:36:46 -0700

    removed title and inserted logo

--- twenty_sixteen/content-page.php
+++ twenty_sixteen/content-page.php
@@ -9,9 +9,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+	/*
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
+	 */
+	?>
 
 	<?php twentysixteen_post_thumbnail(); ?>
 
